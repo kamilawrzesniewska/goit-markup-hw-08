926bc2c941db4ed85687b03c9442b22cd944bdbf
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2022-05-14 14:30:03 +0200

    mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[mobile-menu-open]'),
-    closeModalBtn: document.querySelector('[mobile-menu-close]'),
-    modal: document.querySelector('[mobile-menu]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+    const refs = {
+      openMenuBtn: document.querySelector('[data-menu-open]'),
+      closeMenuBtn: document.querySelector('[data-menu-close]'),
+      modal: document.querySelector('[data-menu]'),
+    };
+  
+    refs.openMenuBtn.addEventListener('click', toggleModal);
+    refs.closeMenuBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
